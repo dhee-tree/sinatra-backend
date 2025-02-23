52a20d55d00f146e66dc5c38bf89c7bf217e2d59
Eliiss <92871000+Eliiss@users.noreply.github.com>
2025-02-23 03:11:32 +0100

    serializer

--- skills/models.py
+++ skills/models.py
@@ -5,6 +5,7 @@ class Skill (models.Model):
     name = models.CharField(max_length=100)
     category = models.CharField(max_length=50, blank=True, null=True) 
     created_at = models.DateTimeField(auto_now_add=True)
+    description = models.CharField(default="", max_length=100)
 
     def __str__(self):
         return self.name
